dc7a5170177f3449db58b98a5fb1fc84a4543de2
BikeXzoZiz <thirachot03@gmail.com>
2023-04-06 00:40:19 +0700

    Update index.php Test

---
+++ component/Navbar.php
@@ -0,0 +1,42 @@
+<nav class="navbar navbar-expand-lg" style="background-color: #005c3f;">
+    <div class="container-fluid">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarContent" aria-controls="navbarContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="th.html" style="color: yellow;">
+                        หน้าแรก
+                    </a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle text-white" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        เกี่ยวกับเรา
+                    </a>
+                    <ul class="dropdown-menu">
+                        <li><a class="dropdown-item" href="#">เรื่องทุเรียน</a></li>
+                    </ul>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-white">สินค้าและบริการ</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-white">ติดต่อเรา</a>
+                </li>
+            </ul>
+            <ul class="navbar-nav mr-auto mb-2 mb-lg-0">
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle text-white" href="#" role="button" data-bs-toggle="dropdownlang" aria-expanded="false">
+                        เลือกภาษา : ไทย
+                    </a>
+                    <ul class="dropdown-menu">
+                        <li><a class="dropdown-item" href="#">中文</a></li>
+                        <li><a class="dropdown-item" href="#">Englist</a></li>
+                        <li><a class="dropdown-item" href="#">ไทย</a></li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
--- index.php
+++ index.php
@@ -1,12 +1,55 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="vๅewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <meta http-equiv="X-UA-Compatible" content="IE=chrome">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> • บริษัท จู ซิน หยวน เทรดดิ้ง จำกัด</title>
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="./css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+    <link rel="stylesheet" href="./css/style.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
 </head>
+
 <body>
-    Hello World
+    <header>
+        <!-- Navbar -->
+        <?php include_once __DIR__ . './component/Navbar.php'; ?>
+    </header>
+    <main class="bg">
+        <div class="container">
+            <div class="row justify-content-center">
+                <img class="" src="./img/QrcodeJuxin.jpg" width="500" height="500" alt="ไม่พบรูปภาพ">
+            </div>
+            <div class="row justify-content-center p-3">
+                <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
+                    <div class="carousel-inner">
+                        <div class="carousel-item active">
+                            <img src="./img/2.png" class="d-block w-50" alt="ไม่พบรูปภาพ" style="margin: auto;">
+                        </div>
+                        <div class="carousel-item">
+                            <img src="./img/3.png" class="d-block w-50" alt="ไม่พบรูปภาพ" style="margin: auto;">
+                        </div>
+                        <div class="carousel-item">
+                            <img src="./img/4.png" class="d-block w-50" alt="ไม่พบรูปภาพ" style="margin: auto;">
+                        </div>
+                        <div class="carousel-item">
+                            <img src="./img/5.png" class="d-block w-50" alt="ไม่พบรูปภาพ" style="margin: auto;">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main>
+    <footer>
+
+    </footer>
+
+    <script src="./js/bootstrap.bundle.min.js"></script>
 </body>
+
 </html>
